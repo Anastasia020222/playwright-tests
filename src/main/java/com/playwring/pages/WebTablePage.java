2c0e8ef1f2d55324014b8102ffa4e719f282ed7e
Anastasia <stasyyyaa@mail.ru>
2024-12-16 16:37:31 +0300

    add-two-test

--- src/main/java/com/playwring/pages/WebTablePage.java
+++ src/main/java/com/playwring/pages/WebTablePage.java
@@ -31,6 +31,7 @@ public class WebTablePage extends AbsBasePage<WebTablePage> {
     Locator department = page.locator("id=department");
     Locator submit = page.locator("id=submit");
     Locator listUser = page.locator("div[role='rowgroup']");
+    Locator iconEdit = page.locator("id=edit-record-1");
 
     @Step("Открытие диалогового окна регистрации пользователя")
     public WebTablePage openDialogAddUser() {
@@ -76,6 +77,52 @@ public class WebTablePage extends AbsBasePage<WebTablePage> {
             assertEquals(t[3], generateUser.getEmail(), "Заданный email не соответствует записи в таблице");
             assertEquals(t[4], generateUser.getSalary(), "Заданный salary не соответствует записи в таблице");
             assertEquals(t[5], generateUser.getDepartment(), "Заданный department не соответствует записи в таблице");
+        });
+        return this;
+    }
+
+    @Step("Открытие диалогового окна редактирования")
+    public WebTablePage clickEditDialogUser() {
+        iconEdit.click();
+        assertTrue(modal.isVisible(), "Диалоговое окно не расскрылось");
+        assertThat(modal).hasClass("fade modal show");
+        String displayStyle = (String) modal.evaluate("el => getComputedStyle(el).display");
+        assertNotEquals("none", displayStyle, "Диалоговое окно не расскрылось");
+        return this;
+    }
+
+    @Step("Введение новых данных")
+    public WebTablePage editFieldsAge() {
+        User generateNewUser = new GenerateUser().generateUser();
+        firstName.fill(generateNewUser.getFirstName());
+        lastName.fill(generateNewUser.getLastName());
+        email.fill(generateNewUser.getEmail());
+        age.fill(generateNewUser.getAge());
+        salary.fill(generateNewUser.getSalary());
+        department.fill(generateNewUser.getDepartment());
+
+        submit.click();
+
+        allureAttachmentText("New user data", generateUser.toString());
+
+        page.waitForCondition(() -> !modal.isVisible());
+
+        assertFalse(modal.isVisible(), "Диалоговое окно не закрылось");
+
+        checkUpdateDataUser(generateNewUser);
+        return this;
+    }
+
+    @Step("Проверяем, что у первого польователя в таблице изменились данные")
+    public WebTablePage checkUpdateDataUser(User generateNewUser) {
+        String[] getInfoUser = listUser.first().innerText().split("\n");
+        Assertions.assertAll("Проверка изменения записей у пользователя в таблице", () -> {
+            assertEquals(getInfoUser[0], generateNewUser.getFirstName(), "FirstName пользователя не соответствует firstname в таблице");
+            assertEquals(getInfoUser[1], generateNewUser.getLastName(), "LastName пользователя не соответствует lastname в таблице");
+            assertEquals(getInfoUser[2], generateNewUser.getAge(), "Заданное age не соответствует age в таблице");
+            assertEquals(getInfoUser[3], generateNewUser.getEmail(), "Заданный email не соответствует записи в таблице");
+            assertEquals(getInfoUser[4], generateNewUser.getSalary(), "Заданный salary не соответствует записи в таблице");
+            assertEquals(getInfoUser[5], generateNewUser.getDepartment(), "Заданный department не соответствует записи в таблице");
         });
         return this;
     }
